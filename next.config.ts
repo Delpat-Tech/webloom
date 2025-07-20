678c144fdbd9485dd0995e5824c26dda3d8107a4
Ashackq <akashpatelyo2@gmail.com>
2025-07-20 20:47:55 +0530

    Sync changes from parent repo 2

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true, // 👈 SKIPS type checking during build
+  },
 };
 
 export default nextConfig;
