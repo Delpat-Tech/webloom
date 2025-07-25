5d70a77f54831e57829f654e56db0d4d532c5be0
Jyoti <jyotisingh016@gmail.com>
2025-07-25 18:37:04 +0530

    fix : modify scrollstack

--- src/components/ui/ScrollStack.tsx
+++ src/components/ui/ScrollStack.tsx
@@ -1,6 +1,6 @@
 import React, { ReactNode, useLayoutEffect, useRef, useCallback } from "react";
 import Lenis from "lenis";
-import {ScrollStackProps, ScrollStackItemProps, CardTransform} from "@/types";
+import {ScrollStackProps, ScrollStackItemProps} from "@/types";
 
 export const ScrollStackItem: React.FC<ScrollStackItemProps> = ({
   children,
@@ -16,6 +16,13 @@ export const ScrollStackItem: React.FC<ScrollStackItemProps> = ({
     {children}
   </div>
 );
+
+interface CardTransform {
+  translateY: number;
+  scale: number;
+  rotation: number;
+  blur: number;
+}
 
 const ScrollStack: React.FC<ScrollStackProps> = ({
   children,
